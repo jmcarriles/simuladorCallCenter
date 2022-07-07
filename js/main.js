0069a5b9147046ef80ecd631a7d4b6b58ec43d32
jmcarriles <jmcarriles@eolos.la>
2022-07-07 19:06:15 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let totalCalls = 150;
+let lostCalls = 34;
+let answeredCalls = 116;
+let queuedCalls = 23; //Llamadas en cola actuales
+let avgWaitingCalls = 120; //Tiempo de espera promedio de una llamada en espera en segundos
+let timeWait;
+let avgLostCalls;
+let avgAnsweredtCalls;
+
+for (let intentos = 0; intentos <= 3; intentos++) {
+
+    let password = parseInt(prompt('Ingrese el password'));
+
+    if (password === 1234) {
+
+        function avgCalls() {
+            avgLostCalls = (lostCalls * 100) / totalCalls;
+            avgAnsweredCalls = (answeredCalls * 100) / totalCalls;
+        }
+
+        timeWait = (queuedCalls * avgWaitingCalls) / 60
+
+
+        avgCalls()
+
+        document.getElementById("totalCalls").innerHTML = totalCalls;
+        document.getElementById("lostCalls").innerHTML = lostCalls;
+        document.getElementById("answeredCalls").innerHTML = answeredCalls;
+        document.getElementById("queuedCalls").innerHTML = queuedCalls;
+        document.getElementById("timeWait").innerHTML = "Tiempo de espera estimado " + timeWait + " minutos";
+        document.getElementById("avgLostCalls").innerHTML = avgLostCalls.toFixed(2) + "%";
+        document.getElementById("avgAnsweredCalls").innerHTML = avgAnsweredCalls.toFixed(2) + "%";
+        break
+    }
+    else {
+
+        alert('Password incorrecto, ingreselo nuevamente.')
+
+    }
+
+    if (intentos === 3) {
+
+        alert('No autorizado')
+        window.location.href = "noautorizado.html";
+    }
+
+}
